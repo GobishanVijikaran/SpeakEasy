5656f46c43dea492d0a533f4dfab1f9e153dfa12
Gobishan Vijikaran <gobishanvijikaran@gvijikaran-MBP16.local>
2021-01-16 17:55:33 -0500

    react router routes for components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import { Grommet } from 'grommet';
 import { grommet } from 'grommet/themes'; 
 import Form from './components/Form'; 
 import Header from './components/Header';
+import CallCenter from './components/Dashboard/CallCenter'; 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Grommet theme={grommet}>
-      <Header />
-      <Form />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Header} />
+            <Route exact path='/user' component={Form} />
+            <Route exact path='/history' component={CallCenter} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     </Grommet>
   );
 }
---
+++ src/components/Dashboard/CallCenter.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function CallCenter() {
+    return (
+        <div className='Call'>
+            <h1>At the call center</h1>
+        </div>
+    )
+}
+
+export default CallCenter
