fcd3b9f959ff6ef6fa3340c1e9636e8d56c741c6
Mihir Kakkar <42092170+MihirKakkar@users.noreply.github.com>
2021-01-16 16:41:06 -0500

    stylesheet

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import './Header.css';
 
 function Header() {
     return (
-        <div>
+        <div id="">
             <h1 id="App-header">Meezer</h1>
-            <p1>A place for people of all ages to connect.</p1>
+            <p1 id="landing-sentence">A place for people of all ages to connect.</p1>
         </div>
     )
 }
