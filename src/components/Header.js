c92ae9e316b88b0f9a0348988243a0b5df830b2f
Gobishan Vijikaran <gobishanvijikaran@gvijikaran-MBP16.local>
2021-01-16 14:47:00 -0500

    basic layout of form

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import { Heading } from 'grommet';
+
+function Header() {
+    return (
+        <div>
+            <Heading>HTN Project</Heading>
+        </div>
+    )
+}
+
+export default Header
