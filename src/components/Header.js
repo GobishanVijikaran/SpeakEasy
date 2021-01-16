ed87ad21b82f227ab911b14a05079108c679d3c0
Mihir Kakkar <42092170+MihirKakkar@users.noreply.github.com>
2021-01-16 18:01:36 -0500

    left and right functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from 'react'; 
 import './Header.css';
+import Form from './Form'; 
 
 function Header() {
     return (
-        <div id="head">
-            <h1 id="App-header">Meezer</h1>
-            <p> </p>
-            <p> </p>
-            <p1 id="landing-sentence">A place for people of all ages to connect.</p1>
+        <div id="splitscreen">
+            <div id="left">
+                <h1 id="App-header">Meezer</h1>
+                <p></p>
+                <p></p>
+                <p1 id="landing-sentence">A place for people of all ages to connect.</p1>
+            </div>
+
+            <div id="right">
+                <Form></Form>
+            </div>
         </div>
+
     )
 }
 
