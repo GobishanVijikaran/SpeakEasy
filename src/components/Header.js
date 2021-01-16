dbb5b2980102a8fec81df8604fedaac91b30a85b
Mihir Kakkar <42092170+MihirKakkar@users.noreply.github.com>
2021-01-16 16:48:27 -0500

    finished titlepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import './Header.css';
 
 function Header() {
     return (
-        <div id="">
+        <div id="head">
             <h1 id="App-header">Meezer</h1>
+            <p> </p>
+            <p> </p>
             <p1 id="landing-sentence">A place for people of all ages to connect.</p1>
         </div>
     )
