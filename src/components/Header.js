50bd4df30f252b1a2b38e7f2662c5c5ac5bddcac
Mihir Kakkar <42092170+MihirKakkar@users.noreply.github.com>
2021-01-17 02:14:01 -0500

    final landing page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import './Header.css';
 import Form from './Form'; 
 import { Grommet } from 'grommet';
 import { grommet } from 'grommet/themes'; 
+import mainLogo from './example-17.png'
 
 function Header() {
     return (
@@ -13,6 +14,8 @@ function Header() {
                 <p></p>
                 <p></p>
                 <p1 id="landing-sentence">Getting seniors connected to a good conversation, anywhere in the world!</p1>
+                <p></p>
+                <img id="imageOne" src={mainLogo} />
             </div>
 
             <div id="right">
