6fa0ec14fb7bf9a9cb829f5c5a3a4ce0ea08d18b
Mihir Kakkar <42092170+MihirKakkar@users.noreply.github.com>
2021-01-17 02:02:36 -0500

    reformat UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,25 @@
 import React from 'react'; 
 import './Header.css';
 import Form from './Form'; 
+import { Grommet } from 'grommet';
+import { grommet } from 'grommet/themes'; 
 
 function Header() {
     return (
+        <Grommet theme={grommet}>
         <div id="splitscreen">
             <div id="left">
-                <h1 id="App-header">Meezer</h1>
+                <h1 id="App-header">SpeakEasy</h1>
                 <p></p>
                 <p></p>
-                <p1 id="landing-sentence">A place for people of all ages to connect.</p1>
+                <p1 id="landing-sentence">Getting seniors connected to a good conversation, anywhere in the world!</p1>
             </div>
 
             <div id="right">
                 <Form></Form>
             </div>
         </div>
-
+        </Grommet>
     )
 }
 
